05ebf1c8e2b16dda0d585464b89f550abd90ca7a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-19 13:20:59 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/recyclerview1005/Monan.java
@@ -0,0 +1,27 @@
+package khoapham.ptp.phamtanphat.recyclerview1005;
+
+public class Monan {
+    private int id;
+    private int hinhanh;
+
+    public Monan(int id, int hinhanh) {
+        this.id = id;
+        this.hinhanh = hinhanh;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHinhanh() {
+        return hinhanh;
+    }
+
+    public void setHinhanh(int hinhanh) {
+        this.hinhanh = hinhanh;
+    }
+}
